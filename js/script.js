a8abe30f5d15756ddf040bfb7de8a6e407c958a5
RedaArsalane <47606983+RedaArsalane@users.noreply.github.com>
2019-11-25 21:07:04 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ function addRecord() {
     var first_name = $("#first_name").val();                        
     var last_name = $("#last_name").val();
     var email = $("#email").val();
-
+    
+    $.post("ajax/addRecord.php", {                                  
+        first_name: first_name,                                     
+        last_name: last_name,
+        email: email
+    }, function (data, status) {                                    
+        $("#add_new_record_modal").modal("hide");                   
+        readRecords();                                              
+        $("#first_name").val("");                                   
+        $("#last_name").val("");
+        $("#email").val("");
+    });
     
 }
 
