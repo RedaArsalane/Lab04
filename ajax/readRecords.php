0b086bbd57ac5041bf4d866a8aa355c225cff3c9
RedaArsalane <arschabani@gmail.com>
2019-11-25 19:33:56 +0100

    read from data base

--- ajax/readRecords.php
+++ ajax/readRecords.php
@@ -1,6 +1,20 @@
 <?php
 
 	include("db_connection.php");			
+		$data = '<table class="table table-bordered table-striped">
+						<tr>
+							<th>No.</th>
+							<th>Name</th>
+							<th>Country</th>
+							<th>Email Address</th>
+							
+						</tr>';
+
+	$query = "SELECT * FROM users";									
+
+	if (!$result = mysqli_query($con, $query)) {					
+        exit(mysqli_error($con
+    }
 	
 							
 ?>
