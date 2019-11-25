c3f747498a090d2f4d6d05d73aedbf6124eb3642
RedaArsalane <47606983+RedaArsalane@users.noreply.github.com>
2019-11-25 20:30:39 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 function addRecord() {
     
+    var first_name = $("#first_name").val();                        
+    var last_name = $("#last_name").val();
+    var email = $("#email").val();
+
+    
 }
 
 function readRecords() {
+     $.get("ajax/readRecords.php", {}, function (data, status) {     
+        $(".records_content").html(data);                           
+    });
    
 }
