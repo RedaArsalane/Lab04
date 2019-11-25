96d50329bbbf723fa1365c68a1cfa557a260c79e
RedaArsalane <arschabani@gmail.com>
2019-11-25 19:24:11 +0100

    index php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>kitchen recipes</title>
+
+    
+    <link rel="stylesheet" type="text/css" href="bootstrap-3.3.5-dist/css/bootstrap.css"/>
+</head>
+<body>
+
