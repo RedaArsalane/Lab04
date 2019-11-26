3c169fcb39658002d1713147fde936620daf3d9c
RedaArsalane <47606983+RedaArsalane@users.noreply.github.com>
2019-11-26 13:18:29 +0100

    Update readUserdetails.php

--- ajax/readUserdetails.php
+++ ajax/readUserdetails.php
@@ -1,6 +1,14 @@
 <?php
 
 include("db_connection.php");                               
+if(isset($_POST['id']) && isset($_POST['id']) != ""){       
+    $user_id = $_POST['id'];                                
+
+    $query = "SELECT * FROM users WHERE id = '$user_id'";   
+    if (!$result = mysqli_query($con, $query)) {              
+        exit(mysqli_error($con));                           
+    }
+ 
 
 
 ?>
